d9897c6304fdf2827152a99ad206c1653d1b46d2
Ayoub METWALLI <ayoub.metwalli@4d.com>
2024-10-25 10:11:25 +0100

    update package and proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { PluginOption, defineConfig } from 'vite';
+import { PluginOption, defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import importMetaUrlPlugin from '@ws-ui/vite-plugins/dist/esbuild-plugin-import-meta-url';
 
-import proxy from './proxy.config';
+const isDevEnv = process.env.NODE_ENV === 'development';
 
-const port = process.env.PORT || 5001;
-const host = process.env.HOST || '0.0.0.0';
+import { initProxy } from './proxy.config';
 
 const redirect = (opts: { from: string; to: string }): PluginOption => {
   return {
@@ -25,20 +25,34 @@ const redirect = (opts: { from: string; to: string }): PluginOption => {
 };
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    redirect({
-      from: '/studio/',
-      to: '/',
-    }),
-  ],
-  define: {
-    'process.env': {},
-  },
-  server: {
-    host,
-    proxy,
-    port: +port,
-  },
+export default defineConfig(({ mode = 'local' }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const port = env.PORT || 5001;
+  const host = env.HOST || '0.0.0.0';
+
+  const proxy = initProxy(env);
+
+  return {
+    plugins: [
+      react(),
+      redirect({
+        from: '/studio/',
+        to: '/',
+      }),
+    ],
+    define: {
+      'process.env': {},
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: isDevEnv ? [importMetaUrlPlugin] : [],
+      },
+    },
+    server: {
+      host,
+      proxy,
+      port: +port,
+    },
+  };
 });
